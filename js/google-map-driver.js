1a9c51b4262030a24e78d1cbe5ee5929acd91ed4
Viraaj Reddi <vishraj55@gmail.com>
2020-06-27 13:11:44 -0700

    added user form to enter pin

---
+++ js/google-map-driver.js
@@ -0,0 +1,35 @@
+function initMap() {
+
+var givenLatitude = parseInt(document.getElementById("forcedLatitude").value);
+var givenLongitude = parseInt(document.getElementById("forcedLongitude").value);
+
+//  var latArray = [40.7608, 40.8608];
+  //var longArray = [-111.8910, -111.9910];
+  var givenCenter = {lat: 40.7608, lng: -111.8910};
+
+
+//latArray.push(givenLatitude);
+//longArray.push(givenLongitude);
+
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: givenCenter
+  });
+
+placeMarker(givenLatitude, givenLongitude, map)
+  //for (i = 0; i < latArray.length; i++) {
+  //  placeMarker(latArray[i], longArray[i], map);
+  //}
+
+}
+
+
+
+function placeMarker(givenLat, givenLong, map) {
+    var marker = new google.maps.Marker({
+        position: {lat: givenLat, lng: givenLong},
+        map: map,
+        draggable:true
+    });
+}
