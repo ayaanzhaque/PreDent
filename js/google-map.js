2d6127f60cad7b9574309a9397c437c3debe395b
Viraaj Reddi <vishraj55@gmail.com>
2020-06-27 11:32:14 -0700

    fixed nav bar

--- js/google-map.js
+++ js/google-map.js
@@ -7,9 +7,45 @@ function initMap() {
     center: {lat: -25.363, lng: 131.044}
   });
 
+for (var i = 0; i < 5; i++) {
+
   var marker = new google.maps.Marker({
-    position: {lat: latArray[0], lng: longArray[0]},
+    position: {lat: latArray[i], lng: longArray[i]},
     map: map,
-    title: 'Hello World!'
+    title: 'Hello World!',
   });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+
+
+}
+
+}
+
+function initMapOriginal() {
+  var latArray = [-25.363, -25.463, -25.563];
+  var longArray = [131.044, 131.144, 131.244];
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: {lat: -25.363, lng: 131.044}
+  });
+
+for (var i = 0; i < 5; i++) {
+
+  var marker = new google.maps.Marker({
+    position: {lat: latArray[i], lng: longArray[i]},
+    map: map,
+    title: 'Hello World!',
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+
+
+}
+
 }
