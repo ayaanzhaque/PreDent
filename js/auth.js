7557b0a7c18d929560d06a3a31ca5e6b0fc3ca66
Adithya Peruvemba <adithya.peruvemba@gmail.com>
2020-06-27 16:41:33 -0700

    Auth JS

--- js/auth.js
+++ js/auth.js
@@ -1,9 +1,27 @@
-const signupForm = document.querySelector('#signup-form')
+/*const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
 
   //user Info
-  const email = signupForm
+  const email = signupForm['signup-email'].value;
+  const password = signupForm["signup-password"].value;
 
-})
+console.log(email, password)
+
+  auth.createUserWithEmailAndPassword(email, password).then(cred =>{
+
+
+  })
+
+
+
+})*/
+
+function myFunction(p1, p2) {
+  const signupForm = document.querySelector('#signup-form');
+  const email = p1;
+  const password = p1;
+  console.log(email, password);
+
+}
