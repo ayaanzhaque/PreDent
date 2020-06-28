0304ef0cadb259b72dcfa337f2a7fca324c4e7cd
Viraaj Reddi <vishraj55@gmail.com>
2020-06-27 17:26:28 -0700

    added infowindows to govt map

--- js/google-map.js
+++ js/google-map.js
@@ -8,7 +8,15 @@ var longArray = [-111.965577, -111.949198, -111.949101, -111.949313, -111.948954
   -111.894109, -111.910951, -111.902568, -111.914381, -111.915209, -111.913635, -111.913138, -111.905964, -111.902885,
   -111.909470, -111.910643, -111.910571, -111.911181, -111.911145, -111.911588, -111.912618];
 
+var contentString = ['<h2>Wah.</h2>', '<h2>Waah.</h2>', '<h2>Waaah.</h2>', '<h2>Waaaaaaaah.</h2>', '<h2>Shaaaa</h2>', '<h2>Uluru</h2>',
+'<h2>Uluru</h2>', '<h2>Waaaaaaaah</h2>', '<h2>Waaaaaaaah</h2>', '<h2>Uluru</h2>', '<h2>Uluru</h2>', '<h2>Shaaaa</h2>', '<h2>Uluru</h2>',
+'<h2>Waaaaaaaah</h2>', '<h2>Uluru</h2>', '<h2>Shaaaa</h2>', '<h2>Uluru</h2>', '<h2>Shaaaa</h2>', '<h2>Uluru</h2>', '<h2>Shaaaa</h2>',
+'<h2>Waaaaaaaah</h2>', '<h2>Shaaaa</h2>', '<h2>Uluru</h2>', '<h2>Shaaaa</h2>'];
+
 var givenCenter = {lat: 40.765287, lng: -111.931735};
+
+
+
 
 
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -17,16 +25,27 @@ var givenCenter = {lat: 40.765287, lng: -111.931735};
   });
 
   for (i = 0; i < latArray.length; i++) {
-  placeMarker(latArray[i], longArray[i], map);
+  placeMarker(latArray[i], longArray[i], map, contentString[0]);
   }
 
 }
 
 
 
-function placeMarker(givenLat, givenLong, map) {
-    var marker = new google.maps.Marker({
-        position: {lat: givenLat, lng: givenLong},
-        map: map
-    });
+function placeMarker(givenLat, givenLong, map, givenContent) {
+
+        var infowindow = new google.maps.InfoWindow({
+          content: givenContent
+        });
+
+        var marker = new google.maps.Marker({
+            position: {lat: givenLat, lng: givenLong},
+            map: map,
+            title: 'GIVEEEEN MEEEAAAPP'
+        });
+
+        marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
+
 }
